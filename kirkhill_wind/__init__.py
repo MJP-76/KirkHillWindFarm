b05af6911e9870dc0e2d1e5c5338203f6eda67e6
matthewjporter <matthew@porterfamily.co.za>
2026-06-29 21:23:45 +0100

    Update __init__.py

--- kirkhill_wind/__init__.py
+++ kirkhill_wind/__init__.py
@@ -1,38 +1,24 @@
-"""The Kirk Hill Wind Farm Integration."""
+"""The Kirk Hill Wind Farm Integration entry point."""
 import logging
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_API_KEY
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
 _LOGGER = logging.getLogger(__name__)
-
 DOMAIN = "kirkhill_wind"
+PLATFORMS = ["sensor"]
 
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_API_KEY): cv.string,
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Kirk Hill Wind Farm from a config entry configured in the UI."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
 
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Kirk Hill component from YAML config."""
-    if DOMAIN not in config:
-        return True
-
-    hass.data[DOMAIN] = {
-        CONF_API_KEY: config[DOMAIN][CONF_API_KEY]
-    }
-
-    # Forward the setup to the sensor platform
-    hass.async_create_task(
-        hass.helpers.discovery.async_load_platform("sensor", DOMAIN, {}, config)
-    )
-
+    # Forward the setup onto the sensor platform
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a Kirk Hill Wind config entry when removed by user."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
