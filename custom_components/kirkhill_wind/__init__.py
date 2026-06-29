e24b01f3b51d6e2f8cb224b8fe74d207d4ce5289
matthewjporter <matthew@porterfamily.co.za>
2026-06-29 22:29:39 +0100

    Update __init__.py

--- custom_components/kirkhill_wind/__init__.py
+++ custom_components/kirkhill_wind/__init__.py
@@ -1,24 +1,13 @@
-"""The Kirk Hill Wind Farm Integration entry point."""
-import logging
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-
-_LOGGER = logging.getLogger(__name__)
+"""The Kirk Hill Wind Farm integration."""
 DOMAIN = "kirkhill_wind"
 PLATFORMS = ["sensor"]
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Kirk Hill Wind Farm from a config entry configured in the UI."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-
-    # Forward the setup onto the sensor platform
+async def async_setup_entry(hass, entry):
+    """Set up Kirk Hill Wind Farm from a config entry."""
+    # Forward the setup to sensor.py
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a Kirk Hill Wind config entry when removed by user."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-    return unload_ok
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
