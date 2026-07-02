4a9795873ab50265834960846d67bbe64066bb8e
matthewjporter <matthew@porterfamily.co.za>
2026-07-02 22:32:26 +0100

    Refactor setup entry and unload entry methods

--- custom_components/kirkhill_wind/__init__.py
+++ custom_components/kirkhill_wind/__init__.py
@@ -1,30 +1,25 @@
-from __future__ import annotations
-
 from .api import KirkHillApi
 from .coordinator import KirkHillCoordinator
+
+DOMAIN = "kirkhill_wind"
+PLATFORMS = ["sensor"]
 
 
 async def async_setup_entry(hass, entry):
     session = hass.helpers.aiohttp_client.async_get_clientsession(hass)
 
-    api = KirkHillApi(
-        entry.data["base_url"],
-        entry.data["api_key"],
-    )
+    api = KirkHillApi(entry.data["base_url"], entry.data["api_key"])
 
-    coordinator = KirkHillCoordinator(
-        hass,
-        api=api,
-        update_interval=60,
-    )
+    coordinator = KirkHillCoordinator(hass, api, 60)
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault("kirkhill", {})[entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setups(
-        entry,
-        ["sensor"],
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
+
+
+async def async_unload_entry(hass, entry):
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
