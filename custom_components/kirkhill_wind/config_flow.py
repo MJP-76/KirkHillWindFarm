2b16c01dcc879125b6f1b826a01b2248ae2591b9
matthewjporter <matthew@porterfamily.co.za>
2026-07-02 22:24:59 +0100

    Refactor KirkHillConfigFlow for API integration

--- custom_components/kirkhill_wind/config_flow.py
+++ custom_components/kirkhill_wind/config_flow.py
@@ -1,29 +1,38 @@
+from homeassistant import config_entries
 import voluptuous as vol
 
-from homeassistant import config_entries
-
-from .const import DOMAIN
+from .api import KirkHillApi
 
 
-class KirkHillConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlow(config_entries.ConfigFlow, domain="kirkhill"):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
-            return self.async_create_entry(
-                title="Kirk Hill Wind Farm",
-                data={
-                    "api_key": user_input["api_key"],
-                },
+            api = KirkHillApi(
+                user_input["base_url"],
+                user_input["api_key"],
             )
 
-        schema = vol.Schema(
-            {
-                vol.Required("api_key"): str,
-            }
-        )
+            session = self.hass.helpers.aiohttp_client.async_get_clientsession(self.hass)
+
+            try:
+                await api.test(session)
+            except Exception:
+                errors["base"] = "cannot_connect"
+            else:
+                return self.async_create_entry(
+                    title="Kirk Hill Wind Farm",
+                    data=user_input,
+                )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
+            data_schema=vol.Schema({
+                vol.Required("base_url"): str,
+                vol.Required("api_key"): str,
+            }),
+            errors=errors,
         )
