23f5bddb0b504d16889d9639e258f29aca5c2c51
matthewjporter <matthew@porterfamily.co.za>
2026-07-02 22:31:12 +0100

    Update config_flow.py

--- custom_components/kirkhill_wind/config_flow.py
+++ custom_components/kirkhill_wind/config_flow.py
@@ -1,32 +1,22 @@
 from homeassistant import config_entries
 import voluptuous as vol
 
-from .api import KirkHillApi
+DOMAIN = "kirkhill_wind"
 
 
-class ConfigFlow(config_entries.ConfigFlow, domain="kirkhill"):
+class KirkHillConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for Kirk Hill Wind Farm."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         errors = {}
 
         if user_input is not None:
-            api = KirkHillApi(
-                user_input["base_url"],
-                user_input["api_key"],
+            return self.async_create_entry(
+                title="Kirk Hill Wind Farm",
+                data=user_input,
             )
-
-            session = self.hass.helpers.aiohttp_client.async_get_clientsession(self.hass)
-
-            try:
-                await api.test(session)
-            except Exception:
-                errors["base"] = "cannot_connect"
-            else:
-                return self.async_create_entry(
-                    title="Kirk Hill Wind Farm",
-                    data=user_input,
-                )
 
         return self.async_show_form(
             step_id="user",
